09e7b6d4b0a91400577eadcdcb6296ffd60ec2fa
MASTERsj01 <154411672+MASTERsj01@users.noreply.github.com>
2024-06-10 03:34:00 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import StaffManagement from './components/StaffManagement.js';
 
 import DutyTask from './components/DutyTask.js';
+import LoginScreen from './components/LoginScreen.js';
+import profile-screen from './components/profile.screen.js';
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
         <Route path="/" element={<DutyTask />} />
         <Route path="/StaffManagement" element={<StaffManagement />} />
         <Route path="/DutyTask" element={<DutyTask />} />
-       
+        <Route path="/LoginScreen" element={<LoginScreen />} />
+        <Route path="/profile-screen" element={<profile-screen />} />
       </Routes>
     </Router>
   );
